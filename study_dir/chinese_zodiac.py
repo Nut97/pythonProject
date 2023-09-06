917e0a19471a492d36cca2ba91b92116e779519a
likuo <nut_97@163.com>
2023-09-06 15:20:24 +0800

    Initial commit

---
+++ study_dir/chinese_zodiac.py
@@ -0,0 +1,6 @@
+# 记录生肖，根据年份来判断生效
+chinese_zodiac="猴鸡狗猪鼠牛虎兔龙蛇马羊"
+year=int(input("请输入你的出生年份:"))
+print(chinese_zodiac[year % 12])
+if ("狗" in chinese_zodiac):
+    print('在十二生肖')
---
+++ study_dir/study_note.py
@@ -0,0 +1,3 @@
+m = map(str,range(1,10))
+print(m)
+print(list(m))
